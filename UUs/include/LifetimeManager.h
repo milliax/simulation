2554b8f081072476d9764a546b57f68523f92fe0
milliax <maxchengboy@gmail.com>
2025-04-17 11:41:22 +0800

    update

---
+++ UUs/include/LifetimeManager.h
@@ -0,0 +1,14 @@
+#ifndef __LIFETIME_MANAGER_H__
+#define __LIFETIME_MANAGER_H__
+
+using namespace std;
+
+class LifetimeManager{
+    private:
+        int a;
+    public:
+        // LifetimeManager() = default;
+        
+};
+
+#endif
--- UUs/src/main.cpp
+++ UUs/src/main.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Hello, World!\n");
+#include "LifetimeManager.h"
 
+int main() {
+    printf("Hello, World!\n");
 
     // ask for the inputs
     // inter-arrival time [a,b]
@@ -11,7 +12,27 @@ int main(){
     // number of servers S
     // number of jobs N
 
-    
+    int a, b, c, d, s, n;
+
+    a = 0;
+    b = 10;
+    c = 0;
+    d = 20;
+
+    s = 1;
+    n = 1;
+
+    int total_time_spent = 0;
+
+    for(int i = 0;i < n;++i){
+        // this is the total job times
+        LifetimeManager env = LifetimeManager();
+
+        
+
+        // total_time_spent += time;
+
+    }
 
     return 0;
 }
